70e76aee087586db232b9bb809c4687f4b8877d9
Caleb Falcão <57542268+Caleb-Falcao@users.noreply.github.com>
2021-10-27 19:20:39 -0300

    site

---
+++ arquivo.js
@@ -0,0 +1,31 @@
+//var n = parseInt(prompt("Informe um valor:"));
+/*while(){
+
+}*/
+
+//for (i = n; i >= 1; i--) {
+//    console.log(i);
+//}
+
+/*// versao em Java Script //FUNCAO DOBRO
+        var umNum, result
+        umNum = parseInt(prompt("informe um numero "));
+        result = dobro(umNum);
+        console.log("O dobro é: ", result);
+        console.log(parOuImpar(umNum));
+        
+        function dobro(n) {
+            let d;
+            d = n * 2
+            return d;
+        } */
+
+        /* FUNCAO PAR IMPAR
+        function parOuImpar(n) {
+            if(n % 2 == 0) {
+                return "O número é par";
+            }
+            else {
+                return "O número é impar";
+            }
+        } */
